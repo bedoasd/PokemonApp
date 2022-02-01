1e187afc83c04134ebd883cd00fcd3cca5c661b3
Bidooooooooooooo <ana_bedo>
2022-02-01 05:22:46 +0200

    Pockemonrx&first build

--- app/src/main/java/com/example/pockemonapp/BaseAplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.pockemonapp;
-
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp   // to make possible for dagger see it from any where
-public class BaseAplication  extends Application {
-}
